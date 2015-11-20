88a4ac573b91576f1621e458e3508e01565306d7
ystkme <bloom6445@gmail.com>
2015-11-21 02:49:19 +0900

    Update setup config

--- setup.sh
+++ setup.sh
@@ -5,9 +5,35 @@ if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
     ln -s ~/dotfiles/prezto ~/.zprezto
 fi
 
-for file in .??*
-do
-  [ "$file" = ".git" ] && continue
+DOT_FILES=(
+  .ctags
+  .editorconfig
+  .gemrc
+  .gitconfig
+  .gitignore_global
+  .irbrc
+  .peco
+  .pryrc
+  .rubocop.yml
+  .sass-lint.yml
+  .scss-lint.yml
+  .tigrc
+  .tmux
+  .tmux.conf
+  .vim
+  .vimrc
+  .zlogin
+  .zlogout
+  .zpreztorc
+  .zshenv
+  .zshrc)
 
-    ln -sv "$HOME/dotfiles/$file" "$HOME/$file"
+for file in ${DOT_FILES[@]}
+do
+  if [ -e $HOME/$file ]; then
+    echo "既にファイルが存在します: $file"
+  else
+    ln -s $HOME/dotfiles/$file $HOME/$file
+    echo "シンボリックリンクを貼りました: $file"
+  fi
 done
